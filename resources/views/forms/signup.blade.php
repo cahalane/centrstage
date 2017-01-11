9a9536e68cfd42532d0f2a9984d667329950b38e
Dave Calnan <dave@jiff.ie>
2017-01-11 21:28:51 +0000

    Edited forms

--- resources/views/forms/signup.blade.php
+++ resources/views/forms/signup.blade.php
@@ -1,18 +1,18 @@
-<form class="signupForm">
+<form class="signupForm" method="POST" action="/signup">
     {{ csrf_field() }}
 
     <div class="signupForm-textInput">
-        <label class="signupForm-textInput-label" for="firstName">First Name</label>
+        <label class="signupForm-textInput-label" for="firstName">First Name:</label>
         <input class="signupForm-textInput-input" type="text" name="firstName" value="{{ old('firstName') }}" validate>
     </div>
 
     <div class="signupForm-textInput">
-        <label class="signupForm-textInput-label" for="lastName">Last Name</label>
+        <label class="signupForm-textInput-label" for="lastName">Last Name:</label>
         <input class="signupForm-textInput-input" type="text" name="lastName" value="{{ old('lastName') }}" validate>
     </div>
 
     <div class="signupForm-textInput">
-        <label class="signupForm-textInput-label" for="email">Email</label>
+        <label class="signupForm-textInput-label" for="email">Email:</label>
         <input class="signupForm-textInput-input" type="text" name="email" value="{{ old('email') }}" validate>
     </div>
 
